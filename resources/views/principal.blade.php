dd872d42d09deae8a7253dec266ae212abffc542
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-04-11 15:53:01 -0300

    feat: add 'principal' route and view

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Principal</title>
+</head>
+<body>
+  <div class="container">
+    <h1> Bienvenido a la página principal</h1>
+    <p> Esta es la página principal de nuestro sitio web. 
+      Aquí puedes encontrar información sobre nuestros servicios, 
+      productos y mucho más. Explora nuestro sitio para descubrir 
+      todo lo que tenemos para ofrecerte. ¡Gracias por visitarnos!</p>
+  </div>
+</body>
+</html>
