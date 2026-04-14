80dfd2f990ece47e4801a5f61fd47857d101f02e
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-04-14 20:10:14 -0300

    Cambio de ruta '/' para que muestre la view 'principal'

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConsultaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
 });
 
 Route::get('/principal', function () {
