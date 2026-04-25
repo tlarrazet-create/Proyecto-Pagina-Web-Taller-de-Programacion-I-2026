454c09b0c5f653a89016679eb1428f791c111713
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-04-25 19:00:36 -0300

    Agrego mas productos al catalogo, los cards quedan desprolijos

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -32,78 +32,144 @@
         <div class="row">
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/escarapela-bandera-mini.jpeg')}}" class="card-img-top" alt="Producto: Escarapela bandera mini">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/escarapela-bandera-mini.jpeg')}}" class="card-img-top img-product" alt="Producto: Escarapela bandera mini">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Escarapela bandera mini</h5>
                 <p class="card-text">De acero. Cierre pin.</p>
-                <span class="badge bg-success fs-6">$2.700</span>
+                <span class="badge bg-success fs-6 mt-auto">$2.700</span>
               </div>
             </div>
           </div>
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/escarapela-circular-mini.jpeg')}}" class="card-img-top" alt="Producto: Escarapela circular mini">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/escarapela-circular-mini.jpeg')}}" class="card-img-top img-product" alt="Producto: Escarapela circular mini">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Escarapela circular mini</h5>
                 <p class="card-text">De acero. Cierre pin.</p>
-                <span class="badge bg-success fs-6">$2.700</span>
+                <span class="badge bg-success fs-6 mt-auto">$2.700</span>
               </div>
             </div>
           </div>
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/abridor-heart-black.jpeg')}}" class="card-img-top" alt="Producto: Abridor heart black">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/abridor-heart-black.jpeg')}}" class="card-img-top img-product" alt="Producto: Abridor heart black">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Abridor heart black</h5>
                 <p class="card-text">Laminados en plata.</p>
-                <span class="badge bg-success fs-6">$4.000</span>
+                <span class="badge bg-success fs-6 mt-auto">$4.000</span>
               </div>
             </div>
           </div>
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/abridor-heart-celeste.jpeg')}}" class="card-img-top" alt="Producto: Abridor heart celeste">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/abridor-heart-celeste.jpeg')}}" class="card-img-top img-product" alt="Producto: Abridor heart celeste">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Abridor heart celeste</h5>
                 <p class="card-text">Laminados en plata.</p>
-                <span class="badge bg-success fs-6">$4.000</span>
+                <span class="badge bg-success fs-6 mt-auto">$4.000</span>
               </div>
             </div>
           </div>
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/abridor-heart-fucsia.jpeg')}}" class="card-img-top" alt="Producto: Abridor heart fucsia">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/abridor-heart-fucsia.jpeg')}}" class="card-img-top img-product" alt="Producto: Abridor heart fucsia">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Abridor heart fucsia</h5>
                 <p class="card-text">Laminados en plata.</p>
-                <span class="badge bg-success fs-6">$4.000</span>
+                <span class="badge bg-success fs-6 mt-auto">$4.000</span>
               </div>
             </div>
           </div>
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/argolla-clasica.jpeg')}}" class="card-img-top" alt="Producto: Argolla clásica">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/argolla-clasica.jpeg')}}" class="card-img-top img-product" alt="Producto: Argolla clásica">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Argolla clásica</h5>
                 <p class="card-text">De plata 925. Tamaño: 10 mm</p>
-                <span class="badge bg-success fs-6 mb-1">Individual:$7.000 / Par:$13.000</span>
+                <span class="badge bg-success fs-6 mt-auto">$7.000 / 2x:$13.000</span>
               </div>
             </div>
           </div>
 
           <div class="col-md-3 mb-4">
-            <div class="card">
-              <img src="{{asset('images/products/argolla-vibe.jpeg')}}" class="card-img-top" alt="Producto: Argolla vibe">
-              <div class="card-body">
+            <div class="card h-100">
+              <img src="{{asset('images/products/argolla-vibe.jpeg')}}" class="card-img-top img-product" alt="Producto: Argolla vibe">
+              <div class="card-body d-flex flex-column">
                 <h5 class="card-title">Argolla vibe</h5>
                 <p class="card-text">De plata 925 con forma de ondas</p>
-                <span class="badge bg-success fs-6">$12.000</span>
+                <span class="badge bg-success fs-6 mt-auto">$12.000</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <div class="card h-100">
+              <img src="{{asset('images/products/pulsera-pandora-heart-black.jpeg')}}" class="card-img-top img-product" alt="Producto: Pulsera Pandora heart black">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">Pulsera Pandora heart black</h5>
+                <p class="card-text">De Acero Quirurgico</p>
+                <span class="badge bg-success fs-6 mt-auto">$6.500 / 2x:$13.000</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <div class="card h-100">
+              <img src="{{asset('images/products/pulsera-pandora-heart.jpeg')}}" class="card-img-top img-product" alt="Producto: Pulsera Pandora heart">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">Pulsera Pandora heart</h5>
+                <p class="card-text">De Acero Quirurgico</p>
+                <span class="badge bg-success fs-6 mt-auto">$6.500 / 2x:$13.000</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <div class="card h-100">
+              <img src="{{asset('images/products/pulsera-colour-azul.jpeg')}}" class="card-img-top img-product" alt="Producto: Pulsera Colour Azul">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">Pulsera Colour Azul</h5>
+                <p class="card-text">De Acero Quirurgico</p>
+                <span class="badge bg-success fs-6 mt-auto">$6.000</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <div class="card h-100">
+              <img src="{{asset('images/products/anillo-hojas.jpeg')}}" class="card-img-top img-product" alt="Producto: Anillo hojas">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">Anillo hojas</h5>
+                <p class="card-text">De Acero Blanco</p>
+                <span class="badge bg-success fs-6 mt-auto">$4.000</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <div class="card h-100">
+              <img src="{{asset('images/products/anillo-julia.jpeg')}}" class="card-img-top img-product" alt="Producto: Anillo Julia">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">Anillo Julia</h5>
+                <p class="card-text">De Acero Blanco</p>
+                <span class="badge bg-success fs-6 mt-auto">$9.100</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <div class="card h-100">
+              <img src="{{asset('images/products/anillo-corazones.jpeg')}}" class="card-img-top img-product" alt="Producto: Anillo Corazones">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">Anillo Corazones</h5>
+                <p class="card-text">De Acero Blanco</p>
+                <span class="badge bg-success fs-6 mt-auto">$3.250</span>
               </div>
             </div>
           </div>
